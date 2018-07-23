8c6594fa9ff31f763f48461abff059ef60d5876c
hx <53434@qq.com>
2018-07-23 14:51:34 +0800

    change deploy ip

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,5 +1,5 @@
 application = ENV['app'] || "etm-frontend" 
-server = ENV['server_app'] || "root@192.168.16.219"
+server = ENV['server_app'] || "root@192.168.224.143"
 set :application, application
 set :stage, :staging
 set :branch, :staging
